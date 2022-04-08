72f0dfb8a59574129c6a9c00ee8a31fcbe8caa51
dddpt <didier.dupertuis@epfl.ch>
2022-04-08 18:24:21 +0200

    added tag_tree creation in DhsTag

--- dhs_scraper/DhsTag.py
+++ dhs_scraper/DhsTag.py
@@ -1,5 +1,13 @@
 import json
 
+
+def tag_tree_create_empty_node(name, parent=None):
+    return {
+        "name": name,
+        "parent": parent,
+        "children": []
+    }
+
 class DhsTag:
     """Defines a DHS tag with properties "tag" and "url"
     
@@ -41,3 +49,48 @@ class DhsTag:
     @staticmethod
     def from_json(json_dict):
         return DhsTag(json_dict["tag"], json_dict["url"])
+
+    @staticmethod
+    def get_tag_tree_node(tag_tree_root, dhs_tag, missing_behaviour=None):
+        levels = dhs_tag.get_levels()
+        current_node=tag_tree_root
+        for l in levels:
+            child_node = None
+            for c in current_node["children"]:
+                if c["name"]==l:
+                    child_node=c
+                    break
+            if child_node is None:
+                if missing_behaviour is None:
+                    return None
+                if missing_behaviour == "create":
+                    child_node = tag_tree_create_empty_node(l, current_node["name"])
+                    current_node["children"].append(child_node)
+                if missing_behaviour == "error":
+                    raise Exception("get_tag_tree_node() non-existent tag tree node for level "+l+" of tag "+dhs_tag) 
+            current_node = child_node
+        return current_node
+
+    @staticmethod
+    def build_tag_tree(tags):
+        root_node = tag_tree_create_empty_node("root")
+        for t in tags:
+            DhsTag.get_tag_tree_node(root_node, t, "create")
+        return root_node
+
+    @staticmethod
+    def get_articles_per_tag(articles):
+        utags = set(t for a in articles for t in a.tags)
+        return [(t, [a for a in articles if t in a.tags]) for t in utags]
+
+    @staticmethod
+    def add_articles_ids_to_tag_tree(tag_tree, articles=None, articles_per_tag=None):
+        if articles is None and articles_per_tag is None:
+            raise Exception("DhsTag.add_articles_ids_to_tag_tree() one of articles or articles_per_tag must be non-None.")
+        if articles_per_tag is None:
+            articles_per_tag = DhsTag.get_articles_per_tag(articles)
+        for t, articles in articles_per_tag:
+            tag_node = DhsTag.get_tag_tree_node(tag_tree, t)
+            if tag_node is None:
+                raise Exception("add_articles_ids_to_tag_tree() non-existent tag tree node for tag: "+t) 
+            tag_node["articles_ids"]=[a.id for a in articles]

1b85eadd9320780b28d927c8cd7b0b66084b7776
dddpt <didier.dupertuis@epfl.ch>
2022-05-23 18:38:12 +0200

    proper DhsTag tag_tree statistics by category

--- dhs_scraper/DhsTag.py
+++ dhs_scraper/DhsTag.py
@@ -75,7 +75,8 @@ class DhsTag:
     @staticmethod
     def get_articles_per_tag(articles):
         utags = set(t for a in articles for t in a.tags)
-        return [(t, [a for a in articles if t in a.tags]) for t in utags]
+        articles_tags = [(a,set(a.tags)) for a in articles]
+        return [(t, [a for a, atags in articles_tags if t in atags]) for t in utags]
 
 
 
@@ -109,6 +110,11 @@ class tag_tree:
     def stats_articles_count(node):
         return [len(node["articles"])] if "articles" in node else [0]
 
+    @staticmethod
+    def stats_aggregator_lists(own_statistics, children_statistics):
+        return [
+            x+sum(cs[i] for cs in children_statistics) for i, x in enumerate(own_statistics)
+        ]
 
     @staticmethod
     def stats_articles_by_category_proportions_curry(article_ids_by_category, categories):
@@ -117,24 +123,29 @@ class tag_tree:
         Tobe used as stat_func inside tag_tree.recursive_node_statistics()
         """
         def articles_by_category_proportions(node):        
-            return [
-                len([a for a in node["articles"] if a.id in article_ids_by_category[c]])
+            return {
+                c: set([a for a in node["articles"] if a.id in article_ids_by_category[c]])
                 for c in categories
-            ]
+            }
         return articles_by_category_proportions
 
+    @staticmethod
+    def stats_aggregator_articles_by_category_proportions(own_statistics, children_statistics):
+        return {
+            c: articles_set.union(*[a_by_c[c] for a_by_c in children_statistics]) for c, articles_set in own_statistics.items()
+        }
 
     @staticmethod
-    def compute_nodes_statistics(tag_tree_root, stat_func=None):
+    def compute_nodes_statistics(tag_tree_root, stat_func=None, stat_aggregator_func=None):
         """Computes 3 statistics for the current node: "statistics", "children_statistics", "total_statistics" """
         if stat_func is None:
             stat_func = tag_tree.stats_articles_count
+        if stat_aggregator_func is None:
+            stat_aggregator_func = tag_tree.stats_aggregator_lists
         def recursive_node_statistics(node, children_statistics):
             node["children_statistics"] = children_statistics
             node["statistics"] = stat_func(node)
-            node["total_statistics"] = [
-                x+sum(cs[i] for cs in children_statistics) for i, x in enumerate(node["statistics"])
-            ]
+            node["total_statistics"] = stat_aggregator_func(node["statistics"], children_statistics)
             return node["total_statistics"]
         return tag_tree.traverse_depth_first(tag_tree_root, recursive_node_statistics)
 
@@ -157,24 +168,23 @@ class tag_tree:
         for t, articles in articles_per_tag:
             tag_node = DhsTag.get_tag_tree_node(tag_tree_root, t)
             if tag_node is None:
-                raise Exception("tag_tree.add_articles_to_tag_tree() non-existent tag tree node for tag: "+t) 
+                raise Exception("tag_tree.add_articles_to_tag_tree() non-existent tag tree node for tag: "+t.__str__()) 
             tag_node["articles"]=[a for a in articles]
-    
-    @staticmethod
-    def modify_articles(tag_tree_root, modifier_func):
-        """Replaces each DhsArticle X by modifier_func(X) in the node["articles"] entry of each node of the tag_tree
+
+    def modify_node_property(tag_tree_root, property, modifier_func):
+        """Replaces each node[property] by modifier_func(node[property])
 
         Modifies the tag_tree_root in place, returns a revert function that, if called, reverts the
-        tag_tree to its original state with each node["articles"] containing the original DhsArticle
+        tag_tree to its original state with each node[property] containing the original content of node[property]
         The revert function assumes the tree structure didn't change (no node added or removed).
         """
         original_articles_sequential = []
         def apply_to_node(node, cr_unused):
-            original_articles_sequential.append(node["articles"])
-            node["articles"] = [modifier_func(a) for a in node["articles"]]
+            original_articles_sequential.append(node[property])
+            node[property] = modifier_func(node[property])
         tag_tree.traverse_depth_first(tag_tree_root, apply_to_node)
         def revert_node(node, cr_unused):
-            node["articles"] = original_articles_sequential.pop(0)
+            node[property] = original_articles_sequential.pop(0)
         def revert():
             tag_tree.traverse_depth_first(tag_tree_root, revert_node)
         return revert

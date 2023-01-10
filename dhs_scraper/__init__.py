d09f834bcb5a534348797de53f0ab9a191a8ff2e
dddpt <didier.dupertuis@epfl.ch>
2023-01-10 10:57:20 +0100

    wikidata bug fix attempt: full path in __init__.py

--- dhs_scraper/__init__.py
+++ dhs_scraper/__init__.py
@@ -1,4 +1,4 @@
 from .DhsArticle import DhsArticle, TOTAL_NB_DHS_ARTICLES, DHS_ARTICLE_CATEGORIES
 from .utils import stream_to_jsonl, lxml_depth_first_iterator
 from .DhsTag import DhsTag, tag_tree
-from .wikidata import *
+from dhs_scraper.wikidata import *
